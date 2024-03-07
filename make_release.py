636565d82eda045266b0ff1690431bcb11d58dd1
DorkmasterFlek <dorkmasterflek@gmail.com>
2024-03-07 16:29:32 -0500

    Make sure manifest version is correct and add some output logging.

--- make_release.py
+++ make_release.py
@@ -23,14 +23,35 @@ parser.add_argument('note', nargs='+', help='The release notes.')
 args = parser.parse_args()
 
 if not re.match(r'^\d+(\.\d+)+$', args.version):
-    raise ValueError('Invalid version number.')
+    parser.error('Invalid version number.')
 
 REPO_NAME = 'SOTN-AP-MapTracker'
 OUTPUT_FILE = REPO_NAME + '.zip'
 
+# Make sure we have GitHub OAuth token from command line first.
+process = subprocess.run(['gh', 'auth', 'token'], capture_output=True, text=True, check=True)
+token = process.stdout.strip()
+
 repo = Repo()
 
+# Make sure manifest version matches the release version.  If not, update it and commit to be in the ZIP.
+with open('manifest.json') as f:
+    manifest = json.load(f)
+
+if manifest['package_version'] != args.version:
+    print(f'Updating manifest.json version to {args.version}')
+    manifest['package_version'] = args.version
+
+    with open('manifest.json', 'w') as f:
+        json.dump(manifest, f, indent=4)
+        f.write('\n')  # Trailing newline.
+
+    repo.index.add(['manifest.json'])
+    repo.index.commit(f'{tag} release')
+    repo.remotes.origin.push()
+
 # Make ZIP file.
+print(f'Creating {OUTPUT_FILE}')
 with open(OUTPUT_FILE, 'wb') as f:
     path = [
         'LICENSE',
@@ -62,10 +83,9 @@ new_version = {
 
 # Publish a new release to GitHub using OAuth token from command line.
 # Use notes as bullet points in the release body.
+print(f'Publishing release {args.version}')
 tag = title = 'v' + args.version
 body = '\n'.join('- ' + n for n in args.note)
-process = subprocess.run(['gh', 'auth', 'token'], capture_output=True, text=True, check=True)
-token = process.stdout.strip()
 
 g = Github(token)
 remote_repo = g.get_user().get_repo(REPO_NAME)
@@ -76,6 +96,7 @@ attachment = release.upload_asset(OUTPUT_FILE, label=OUTPUT_FILE)
 new_version['download_url'] = attachment.browser_download_url
 
 # Add download URL to new versions entry at the front and commit it to the repository.
+print(f'Updating versions.json')
 versions['versions'].insert(0, new_version)
 with open('versions.json', 'w') as f:
     json.dump(versions, f, indent=4)
@@ -84,3 +105,6 @@ with open('versions.json', 'w') as f:
 repo.index.add(['versions.json'])
 repo.index.commit(f'{tag} release')
 repo.remotes.origin.push()
+
+# Done.
+print(f'Release {args.version} published')
